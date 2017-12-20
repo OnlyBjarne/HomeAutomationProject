40f1e2ee4ba78e2bb9e6e846a0b9895e532c0f4c
OnlyBjarne <bjakke112@gmail.com>
2017-12-20 16:07:40 +0100

    Homescreen weather-card is finished for now

--- static/scripts.js
+++ static/scripts.js
@@ -3,12 +3,18 @@ $(document).ready(function () {
 });
 
 function getWeatherNow() {
-    $.get('/weather', function (result) {
-        var symbol = result['symbol']['@numberEx'];
-        var temp = result['temperature']['@value'];
-        var windDir = result['windDirection']['@code'];
-        var  windSpeed = result['windSpeed']['@mps'];
-        var downfall = result['precipitation']['@value'];
-        alert(symbol);
+    $.getJSON('/weather', function (data) {
+        var result = [
+            data['symbol']['@var'],
+            data['temperature']['@value'],
+            data['windDirection']['@code'],
+            data['windSpeed']['@mps'],
+            data['precipitation']['@value']
+        ];
+        $( "#weathericon" ).attr("src", "https://www.yr.no/grafikk/sym/v2017/png/100/"+result[0]+".png");
+        $( "#tempData" ).html(result[1]+"&degC");
+        $( "#perciData" ).html(result[4]+" mm");
+        $( "#windData" ).html(result[3]+"m/s fra "+result[2]);
     });
+    setTimeout(getWeatherNow, 10*60*1000);
 }
